01ed434cc3efb3f552e75dc9516b485b6e1e6cfc
B-loeb <bradleyloeb@gmail.com>
2022-03-08 00:36:30 -0500

    more assets

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas. width = 800;
+canvas. width = 1000;
 canvas.height = 500;
 
+  
+  
 //keep track of what keys the user presses on keyboard using keyUP and keyDown event Listeners GLOBAL
 const globalKey = [];
 
@@ -11,8 +13,21 @@ const globalKey = [];
 cons player = {
     x: 0,
     y: 0,
-    width: ???;
-    height: ???;
+    width: 32;
+    height: 36;
     frameX: 0,
     frameY: 0,
+    speed: 9;
+    moving: false;
 }
+
+const userSprite = new Image();
+userSprite.src = "MilesofStones\assets\Images\characterSprite\emaleLead.png";
+const background = new Image();
+background.src = "MilesofStones\assets\Images\BackgroundSelfDrawMoon.png"
+
+//animation loop
+function animate(){
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+}
+animate()
